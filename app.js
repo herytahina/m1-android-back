654fc6b14195d1f7f4dc6d75de1785ee32bce532
Hery Tahina Rakotonaivo <heryrak.hr@gmail.com>
2023-08-03 18:11:29 +0300

    init project

---
+++ app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`App running on port ${port}!`));
