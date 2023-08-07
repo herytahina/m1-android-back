94f3a6b27d771afcfdbd881373eaa448aa9c2791
Hery Tahina Rakotonaivo <heryrak.hr@gmail.com>
2023-08-07 21:15:53 +0300

    Update database.js

--- database.js
+++ database.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME
 }).promise()
 
 module.exports = { 
